d36b720c2c3d4a04df4bf4ed95925f5e5b0e153c
ofs416 <ofs416@users.noreply.github.com>
2024-12-13 19:58:33 +0000

    Style: Format code with Ruff

--- Misc.py
+++ Misc.py
@@ -1,21 +1,29 @@
 import numpy as np
-import matplotlib.pyplot as plt  
+import matplotlib.pyplot as plt
 from typing import Optional, List
 
-def energy_cons_check(trajectory: np.ndarray, g: float, length: float, mass: float) -> None:
+
+def energy_cons_check(
+    trajectory: np.ndarray, g: float, length: float, mass: float
+) -> None:
     gp = mass * g * length * (1 + np.cos(trajectory[:, 0]))
     ke = 0.5 * mass * length * trajectory[:, 1] ** 2
     total_energy = gp + ke
     print(total_energy.max() / total_energy[0])
 
 
-def plot_state(xaxis: np.ndarray, yaxis: np.ndarray, title: Optional[str], legends: Optional[List[str]]) -> None:
+def plot_state(
+    xaxis: np.ndarray,
+    yaxis: np.ndarray,
+    title: Optional[str],
+    legends: Optional[List[str]],
+) -> None:
     plt.figure(figsize=(12, 6))
-    for i in range(yaxis.shape[1]): 
+    for i in range(yaxis.shape[1]):
         if legends:
             plt.plot(xaxis, yaxis[:, i], label=legends[i])
             plt.legend()
-        else: 
+        else:
             plt.plot(xaxis, yaxis[:, i])
     plt.title(title)
     plt.xlabel("Time (s)")
